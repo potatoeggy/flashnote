40588ef023ff2b5dc897177ff2829b38c9b881bc
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2021-01-16 11:56:50 -0500

    sample call for cards

--- app/src/main/java/com/flashnote/data/ApiService.java
+++ app/src/main/java/com/flashnote/data/ApiService.java
@@ -18,21 +18,22 @@ public class ApiService {
     private final static String DROPBASE_PIPELINE = "v1/pipeline/";
     private final static String DROPBASE_AUTH = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhYmFzZUlkIjoiYldUUkI3aVN3YXp6dWI0UDdIWm5RUyIsImFjY2Vzc1Blcm0iOiJmdWxsIiwidG9rZW5JZCI6IjlsQTJaaFBZOWhkWFl6WXFtWUxPdWp2RWhIUWU3QnJBcW5LRTE0YWd0dHJCTXV2ZEt0NkNzQXB5VmYxcmNMbGEiLCJpYXQiOjE2MTA3NzUzNDIsImV4cCI6MTYxMTcyNTc0MiwiaXNzIjoiZHJvcGJhc2UuaW8iLCJzdWIiOiJLemJEeGpGVWZ0eDJtOFV5Vld6SkJrIn0.YvlWqXNZq5NtwAMOzrijwhsi0z1Joc74BsKH_5_PW7g";
     
-    public static Call<List<Card>> getCardsByTag(Tag tag) throws Exception { // hard
-        List<Tag> singleTag = new ArrayList<Tag>();
-        singleTag.add(tag);
-        return null;
+    public static Call<List<Card>> getCardsByIdMap(List<IdMap> idMaps)  { // hard
+        List<String> cardIds = new ArrayList<String>();
+        for (IdMap map : idMaps) {
+            cardIds.add(map.getCardId());
+        }
+        Call<List<Card>> cardCall = restService.getCardsById(DROPBASE_DB, DROPBASE_AUTH, cardIds);
+        return cardCall;
     }
 
-    public static Call<List<IdMap>> getCardsByTag(List<Tag> tags) throws Exception {
+    public static Call<List<IdMap>> getIdMapByTag(List<Tag> tags) throws Exception {
         List<String> tagIds = new ArrayList<String>();
         for (Tag t : tags) {
             if (t.getId() == null) throw new Exception("i'll fix later please never run into this");
             tagIds.add(t.getId());
         }
-
         Call<List<IdMap>> idMapCall = restService.getMapByTagId(DROPBASE_DB, DROPBASE_AUTH, tagIds);
-
         return idMapCall;
     }
     
@@ -68,10 +69,41 @@ public class ApiService {
         List<Tag> tags = new ArrayList<Tag>();
         tags.add(new Tag("bleh", "bleh", "bleh"));
         tags.get(0).setId("06ec495e-c36a-4a62-a41e-aeebe35d9bb2");
+
         try {
-            for (Card c : getCardsByTag(tags)) {
-                System.out.println(c);
-            }
+            Call<List<IdMap>> map = getIdMapByTag(tags);
+            map.enqueue(new Callback<List<IdMap>>() {
+                @Override
+                public void onResponse(Call<List<IdMap>> call, Response<List<IdMap>> response) {
+                    if (response.isSuccessful()) {
+                        Call<List<Card>> cards = getCardsByIdMap(response.body());
+                        cards.enqueue(new Callback<List<Card>>() {
+                            @Override
+                            public void onResponse(Call<List<Card>> call, Response<List<Card>> response) {
+                                if (response.isSuccessful()) {
+                                    for (Card c : response.body()) {
+                                        System.out.println(c.toString());
+                                    }
+                                } else {
+                                    System.out.println(response.body().get(0));
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<List<Card>> call, Throwable t) {
+                                t.printStackTrace();
+                            }
+                        });
+                    } else {
+                        System.out.println("ERROR 1: " + response.toString());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<IdMap>> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
             System.out.println(e.fillInStackTrace());
