9fb80b6a0dfad32bc8f811c668e6e787919ff649
Vincent Guo <vincentguo04@gmail.com>
2021-01-15 23:29:19 -0500

    Changed Package Hierarchy. Everything should be under com.flashnote (#3)

--- app/src/main/java/com/flashnote/main/MainActivity.java
+++ app/src/main/java/com/flashnote/main/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.flashnote;
+package com.flashnote.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.flashnote.R;
 
 public class MainActivity extends AppCompatActivity {
 	
