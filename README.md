142de9b2ee55835b59178668354ba83bd6741e9b
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2021-01-16 14:03:36 -0500

    revert everything

--- README.md
+++
@@ -1,2 +0,0 @@
-# flashnote
-Convert and share all of your notes as simple flashcards
--- app/src/main/java/com/flashnote/main/State.java
+++
@@ -1,9 +0,0 @@
-package com.example.flashnote;
-
-import java.util.ArrayList;
-
-public class State {
-    static ArrayList<Card> cards = new ArrayList<>();
-    static ArrayList<Card> playing;
-    static int playingIndex;
-}
